92f8cfae6cf03bb849ececf8b17eada0f1445802
xxMUGIWARAxx <misha_grigoryan_83@mail.ru>
2025-07-13 20:53:14 +0300

    second commit

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,21 +1,23 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarServiceImpl implements CarService {
-    private static int CARS_COUNT;
+    private int carCount;
     private List<Car> cars;
 
     public CarServiceImpl() {
         cars = new ArrayList<>();
-        cars.add(new Car(++CARS_COUNT, "Toyota", 186));
-        cars.add(new Car(++CARS_COUNT, "BMW", 350));
-        cars.add(new Car(++CARS_COUNT, "Honda", 144));
-        cars.add(new Car(++CARS_COUNT, "Audi", 1169));
-        cars.add(new Car(++CARS_COUNT, "Mercedes", 210));
+        cars.add(new Car(++carCount, "Toyota", 186));
+        cars.add(new Car(++carCount, "BMW", 350));
+        cars.add(new Car(++carCount, "Honda", 144));
+        cars.add(new Car(++carCount, "Audi", 1169));
+        cars.add(new Car(++carCount, "Mercedes", 210));
     }
 
     @Override
